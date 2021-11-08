a9f6496f80d1d52ec72f1869a71897b8aab14723
Gabriel DP <gabrieldominguezprado@gmail.com>
2021-11-08 02:02:40 -0300

    Despliegue country app 08-11-2021

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.get('*', (req, res) => {
  
 app.listen(port, () => {
 
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Servidor corriendo en http://localhost:${port}.`)
 
   });
